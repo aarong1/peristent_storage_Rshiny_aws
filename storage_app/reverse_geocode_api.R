50033ca4fa0d98355cbc7b64e355fbb762d56502
gormanaa@tcd.ie <gormanaa@tcd.ie>
2022-01-08 21:22:46 +0000

    docs added+minor aesthetics

--- storage_app/reverse_geocode_api.R
+++ storage_app/reverse_geocode_api.R
@@ -2,14 +2,15 @@
 #----------------
 #https://locationiq.com/sandbox/geocoding/reverse
 library(httr)
+key <- Sys.getenv('api_key')
 url <- function(lat='54.53',lon='-5.31'){
-url <- paste0('https://us1.locationiq.com/v1/reverse.php?key=pk.60846804b3122e58f24c91674fbeddf0&',
+  url <- paste0('https://us1.locationiq.com/v1/reverse.php?key=',key,'&',
 'lat=',lat,
 '&',
 'lon=',lon,
 '&format=json')
 }
 
-response <- VERB('GET',url)
+response <- VERB('GET',url())
 
 content(response)
